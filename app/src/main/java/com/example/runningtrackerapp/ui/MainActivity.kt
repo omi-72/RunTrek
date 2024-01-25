adab19efceb4f55d94cc86a95fa9a460762151a0
omi-72 <jannat.omi722@gmail.com>
2024-01-25 19:10:49 +0600

    create nav graph

--- app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningtrackerapp/ui/MainActivity.kt
@@ -3,20 +3,38 @@ package com.example.runningtrackerapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.runningtrackerapp.R
+import com.example.runningtrackerapp.databinding.ActivityMainBinding
 import com.example.runningtrackerapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
-    @Inject
-    lateinit var runDao: RunDao
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        setSupportActionBar(binding.toolbar)
+        binding.bottomNavigationView.setupWithNavController(binding.navHostFragment.findNavController())
+        binding.navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment , R.id.runFragment2, R.id.statisticsFragment ->
+                        binding.bottomNavigationView.visibility =
+                        View.GONE
+                    else -> binding.bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
 
     }
 }
