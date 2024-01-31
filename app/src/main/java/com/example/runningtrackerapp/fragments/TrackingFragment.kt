a071ee21be2e52ef3688b0143663acca15caf613
omi-72 <jannat.omi722@gmail.com>
2024-01-31 15:19:35 +0600

    Tracking service setup and continue with forgeround service

--- app/src/main/java/com/example/runningtrackerapp/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtrackerapp/fragments/TrackingFragment.kt
@@ -1,5 +1,6 @@
 package com.example.runningtrackerapp.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningtrackerapp.R
 import com.example.runningtrackerapp.databinding.FragmentTrackingBinding
+import com.example.runningtrackerapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.runningtrackerapp.services.TrackingService
 import com.example.runningtrackerapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,10 +37,19 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         binding.mapView.onCreate(savedInstanceState)
 
+        binding.btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         binding.mapView.getMapAsync {
             map = it
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
